67ca046b5c64214e65f398c21179726c7ee7ab50
Vinai Kopp <vinai@netzarbeiter.com>
2011-09-07 22:23:22 +0200

    handle cache invalidation when visibility settings are changed

--- app/code/community/Netzarbeiter/GroupsCatalog2/Model/Observer.php
+++ app/code/community/Netzarbeiter/GroupsCatalog2/Model/Observer.php
@@ -49,6 +49,37 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 	}
 
 	/**
+	 * Set the html block cache to disabled/invalid when groupscatalog visibility settings
+	 * have changed because the top menu is cached.
+	 *
+	 * This might be better suited to go into the indexer, but that seems very unclean to mix
+	 * the indexer logic with the cache logic. So for now I'll put it here.
+	 *
+	 * @param Varien_Event_Observer $observer
+	 * @return void
+	 */
+	public function catalogCategorySaveAfter(Varien_Event_Observer $observer)
+	{
+		/* @var $category Mage_Catalog_Model_Category */
+		$category = $observer->getCategory();
+		/* @var $helper Netzarbeiter_GroupsCatalog2_Helper_Data */
+		$helper = Mage::helper('netzarbeiter_groupscatalog2');
+
+		if ($category->dataHasChangedFor(Netzarbeiter_GroupsCatalog2_Helper_Data::HIDE_GROUPS_ATTRIBUTE))
+		{
+			if ($helper->getConfig('auto_refresh_block_cache'))
+			{
+				// Only refresh the category block cache: Mage_Catalog_Model_Category::CACHE_TAG
+				Mage::app()->cleanCache(array(Mage_Catalog_Model_Category::CACHE_TAG));
+			}
+			else
+			{
+				Mage::app()->getCacheInstance()->invalidateType(Mage_Core_Block_Abstract::CACHE_GROUP);
+			}
+		}
+	}
+
+	/**
 	 * "Unload" the specified catalog entity if the groupscatalog settings specify so
 	 * 
 	 * @param Mage_Catalog_Model_Abstract $entity
