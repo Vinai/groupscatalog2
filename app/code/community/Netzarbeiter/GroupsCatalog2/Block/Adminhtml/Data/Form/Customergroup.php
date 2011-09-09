4e302e4d88c769967cbc5647cf80ba9297ad01b5
Vinai Kopp <vinai@netzarbeiter.com>
2011-09-09 10:18:18 +0200

    Add the license comment header to every file

--- app/code/community/Netzarbeiter/GroupsCatalog2/Block/Adminhtml/Data/Form/Customergroup.php
+++ app/code/community/Netzarbeiter/GroupsCatalog2/Block/Adminhtml/Data/Form/Customergroup.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Netzarbeiter
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category   Netzarbeiter
+ * @package    Netzarbeiter_GroupsCatalog2
+ * @copyright  Copyright (c) 2011 Vinai Kopp http://netzarbeiter.com
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class Netzarbeiter_GroupsCatalog2_Block_Adminhtml_Data_Form_Customergroup
 	extends Varien_Data_Form_Element_Multiselect
