0d706dcf07a4c07136a27fb866a3a207209a42db
Vinai Kopp <vinai@netzarbeiter.com>
2011-09-09 10:17:26 +0200

    Add the wishlist item collection rewrite replacement class

---
+++ app/code/community/Netzarbeiter/GroupsCatalog2/Model/Wishlist/Resource/Item/Collection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Netzarbeiter
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category   Netzarbeiter
+ * @package    Netzarbeiter_GroupsCatalog2
+ * @copyright  Copyright (c) 2011 Vinai Kopp http://netzarbeiter.com
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+ 
+class Netzarbeiter_GroupsCatalog2_Model_Wishlist_Resource_Item_Collection
+	extends Mage_Wishlist_Model_Resource_Item_Collection
+{
+	/**
+	 * Name prefix of events that are dispatched by model
+	 *
+	 * @var string
+	 */
+	protected $_eventPrefix = 'wishlist_item_collection';
+
+	/**
+	 * Name of event parameter
+	 *
+	 * @var string
+	 */
+	protected $_eventObject = 'collection';
+}
