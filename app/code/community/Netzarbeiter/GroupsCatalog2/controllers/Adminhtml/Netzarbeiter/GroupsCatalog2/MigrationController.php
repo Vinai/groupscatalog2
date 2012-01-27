6cf9d860db16fb7ae81f3713a19ae7055d37154a
Vinai Kopp <vinai@netzarbeiter.com>
2012-01-27 10:45:37 +0100

    Mark active menu in admin migration controller

--- app/code/community/Netzarbeiter/GroupsCatalog2/controllers/Adminhtml/Netzarbeiter/GroupsCatalog2/MigrationController.php
+++ app/code/community/Netzarbeiter/GroupsCatalog2/controllers/Adminhtml/Netzarbeiter/GroupsCatalog2/MigrationController.php
@@ -1,35 +1,36 @@
 <?php
- 
+
 class Netzarbeiter_GroupsCatalog2_Adminhtml_Netzarbeiter_GroupsCatalog2_MigrationController
-	extends Mage_Adminhtml_Controller_Action
+    extends Mage_Adminhtml_Controller_Action
 {
-	public function indexAction()
-	{
-		$this->loadLayout()->renderLayout();
-	}
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->_setActiveMenu('system/tools/netzarbeiter_groupscatalog2');
+        $this->renderLayout();
+    }
 
-	public function doStepAction()
-	{
-		try
-		{
-			$step = $this->getRequest()->getParam('migration_step');
-			if (! $step)
-			{
-				Mage::throwException($this->__('No migration step specified.'));
-			}
-			Mage::helper('netzarbeiter_groupscatalog2/migration')->doStep($step);
-			
-			$this->_getSession()->addSuccess($this->__('Finished migration step "%s" successfully.', $step));
-		}
-		catch (Exception $e)
-		{
-			$this->_getSession()->addError($e->getMessage());
-		}
-		$this->_redirect('*/*/index');
-	}
+    public function doStepAction()
+    {
+        try
+        {
+            $step = $this->getRequest()->getParam('migration_step');
+            if (!$step) {
+                Mage::throwException($this->__('No migration step specified.'));
+            }
+            Mage::helper('netzarbeiter_groupscatalog2/migration')->doStep($step);
 
-	protected function _isAllowed()
-	{
-		return Mage::getSingleton('admin/session')->isAllowed('system/tools/netzarbeiter_groupscatalog2');
-	}
+            $this->_getSession()->addSuccess($this->__('Finished migration step "%s" successfully.', $step));
+        }
+        catch (Exception $e)
+        {
+            $this->_getSession()->addError($e->getMessage());
+        }
+        $this->_redirect('*/*/index');
+    }
+
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('system/tools/netzarbeiter_groupscatalog2');
+    }
 }
