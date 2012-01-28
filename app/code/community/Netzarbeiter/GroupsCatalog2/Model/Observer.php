f85a2f49a10db116a83d5ef25085247fbe61788e
Vinai Kopp <vinai@netzarbeiter.com>
2012-01-28 17:18:59 +0100

    Added redirect on access to hidden entity feature

--- app/code/community/Netzarbeiter/GroupsCatalog2/Model/Observer.php
+++ app/code/community/Netzarbeiter/GroupsCatalog2/Model/Observer.php
@@ -15,11 +15,11 @@
  * newer versions in the future.
  *
  * @category   Netzarbeiter
- * @package    Netzarbeiter_GroupsCatalog2
+ * @package	Netzarbeiter_GroupsCatalog2
  * @copyright  Copyright (c) 2012 Vinai Kopp http://netzarbeiter.com
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
- 
+
 class Netzarbeiter_GroupsCatalog2_Model_Observer
 {
 	/**
@@ -54,7 +54,12 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 	 */
 	public function catalogCategoryLoadAfter(Varien_Event_Observer $observer)
 	{
-		$this->_applyGroupsCatalogSettingsToEntity($observer->getCategory());
+		$category = $observer->getCategory();
+		$this->_applyGroupsCatalogSettingsToEntity($category);
+		if ($category->getData('forbidden_by_groupscatalog2'))
+		{
+			$this->_applyHiddenEntityHandling(Mage_Catalog_Model_Product::ENTITY);
+		}
 	}
 
 	/**
@@ -65,7 +70,84 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 	 */
 	public function catalogProductLoadAfter(Varien_Event_Observer $observer)
 	{
-		$this->_applyGroupsCatalogSettingsToEntity($observer->getProduct());
+		$product = $observer->getProduct();
+		$this->_applyGroupsCatalogSettingsToEntity($product);
+		if ($product->getData('forbidden_by_groupscatalog2'))
+		{
+			$this->_applyHiddenEntityHandling(Mage_Catalog_Model_Product::ENTITY);
+		}
+	}
+
+	/**
+	 * Apply the message display and redirect if configured.
+	 * Currently there is no differentiation between products and categories here, so
+	 * the entityTypeCode parameter is ignored. Might be used in future if requested.
+	 *
+	 * @param string $entityTypeCode
+	 */
+	protected function _applyHiddenEntityHandling($entityTypeCode)
+	{
+		$helper = $this->_getHelper();
+		if ($helper->isModuleActive() && !$this->_isApiRequest())
+		{
+			$this->_applyHiddenEntityMsg($entityTypeCode);
+			$this->_applyHiddenEntityRedirect($entityTypeCode);
+		}
+	}
+
+	/**
+	 * Apply the configured splash message to display if a hidden entity is accessed.
+	 * The passed in entity type code is currently being ignored.
+	 *
+	 * @param string $entityTypeCode
+	 */
+	protected function _applyHiddenEntityMsg($entityTypeCode)
+	{
+		$helper = $this->_getHelper();
+		if ($helper->getConfig('display_entity_hidden_msg'))
+		{
+			if (Mage::getSingleton('customer/session')->isLoggedIn())
+			{
+				$message = $helper->getConfig('entity_hidden_msg_customer');
+			}
+			else
+			{
+				$message = $helper->getConfig('entity_hidden_msg_guest');
+			}
+			if (mb_strlen($message, 'UTF-8') > 0)
+			{
+				Mage::getSingleton('core/session')->addError($message);
+			}
+		}
+	}
+
+	/**
+	 * Apply redirects for hidden entity page requests if configured.
+	 *
+	 * @param string $entityTypeCode
+	 */
+	protected function _applyHiddenEntityRedirect($entityTypeCode)
+	{
+		$helper = $this->_getHelper();
+		if (Mage::getSingleton('customer/session')->isLoggedIn())
+		{
+			$handlingTypeSetting = 'entity_hidden_behaviour_customer';
+			$targetRouteSetting = 'entity_hidden_redirect_customer';
+		}
+		else
+		{
+			$handlingTypeSetting = 'entity_hidden_behaviour_guest';
+			$targetRouteSetting = 'entity_hidden_redirect_guest';
+		}
+		$type = $helper->getConfig($handlingTypeSetting);
+		if (Netzarbeiter_GroupsCatalog2_Model_System_Config_Source_HiddenEntityHandling::HIDDEN_ENTITY_HANDLING_REDIRECT == $type)
+		{
+			$route = Mage::getUrl($helper->getConfig($targetRouteSetting));
+			Mage::app()->getResponse()
+				->setRedirect($route)
+				->sendHeaders();
+			Mage::app()->getRequest()->setDispatched(true);
+		}
 	}
 
 	/**
@@ -85,7 +167,7 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 		$helper = $this->_getHelper();
 
 		// If the module isn't disabled on a global scale
-		if ($helper->isModuleActive($category->getStore(), false) && ! $this->_isApiRequest())
+		if ($helper->isModuleActive($category->getStore(), false) && !$this->_isApiRequest())
 		{
 			if ($category->dataHasChangedFor(Netzarbeiter_GroupsCatalog2_Helper_Data::HIDE_GROUPS_ATTRIBUTE))
 			{
@@ -104,10 +186,10 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 
 	/**
 	 * Add the groupscatalog filter to the wishlist item collection.
-	 * 
+	 *
 	 * This event only exists because of the rewrite of the wishlist item collection. The event
 	 * prefix and object properties are not set in the core. A contribution patch is on its way, though.
-	 * 
+	 *
 	 * @param Varien_Event_Observer $observer
 	 * @return void
 	 */
@@ -117,7 +199,7 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 		$collection = $observer->getCollection();
 		$helper = $this->_getHelper();
 		$store = Mage::app()->getStore();
-		if ($helper->isModuleActive($store) && ! $this->_isApiRequest())
+		if ($helper->isModuleActive($store) && !$this->_isApiRequest())
 		{
 			$customerGroupId = $helper->getCustomerGroupId();
 			Mage::getResourceSingleton('netzarbeiter_groupscatalog2/filter')
@@ -128,7 +210,7 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 	/**
 	 * Add the groupscatalog filter to the product collection count so the numbers
 	 * beside the categories in the sidebar navigation are correct.
-	 * 
+	 *
 	 * @param Varien_Event_Observer $observer
 	 * @return void
 	 */
@@ -137,7 +219,7 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 		/* @var $collection Mage_Catalog_Model_Resource_Product_Collection */
 		$collection = $observer->getCollection();
 		$helper = $this->_getHelper();
-		if ($helper->isModuleActive($collection->getStoreId()) && ! $this->_isApiRequest())
+		if ($helper->isModuleActive($collection->getStoreId()) && !$this->_isApiRequest())
 		{
 			$customerGroupId = $helper->getCustomerGroupId();
 			Mage::getResourceSingleton('netzarbeiter_groupscatalog2/filter')
@@ -148,12 +230,12 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 	/**
 	 * Update the quote items quantities, in case one of the quote item products has been hidden.
 	 * If we don't do that the sidebar cart item qty might be wrong.
-	 * 
+	 *
 	 * There might be a better way to do this but so far this is the best way I could think of.
 	 * This is a very rare case that probably won't come into effect at all, only when a product
 	 * that a customer has in the cart becomes hidden (may be a customer group change or a product
 	 * setting change). Maybe this can go if the overhead is too large. Leave for now.
-	 * 
+	 *
 	 * @param Varien_Event_Observer $observer
 	 * @return void
 	 * @see Mage_Sales_Model_Quote::collectTotals()
@@ -168,27 +250,34 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 		 * recalculate all totals here, we just need to make sure the item quantities are correct.
 		 */
 		if ($this->_getHelper()->isModuleActive($quote->getStore()) &&
-			$quote->getItemsQty() > 0 && ! $this->_isApiRequest())
+			$quote->getItemsQty() > 0 && !$this->_isApiRequest()
+		)
 		{
 			$itemsCount = $itemsQty = $virtualItemsQty = 0;
-			foreach ($quote->getAllVisibleItems() as $item) {
-				if ($item->getParentItem()) {
+			foreach ($quote->getAllVisibleItems() as $item)
+			{
+				if ($item->getParentItem())
+				{
 					continue;
 				}
 				$children = $item->getChildren();
-				if ($children && $item->isShipSeparately()) {
-					foreach ($children as $child) {
-						if ($child->getProduct()->getIsVirtual()) {
-							$virtualItemsQty += $child->getQty()*$item->getQty();
+				if ($children && $item->isShipSeparately())
+				{
+					foreach ($children as $child)
+					{
+						if ($child->getProduct()->getIsVirtual())
+						{
+							$virtualItemsQty += $child->getQty() * $item->getQty();
 						}
 					}
 				}
-				if ($item->getProduct()->getIsVirtual()) {
+				if ($item->getProduct()->getIsVirtual())
+				{
 					$virtualItemsQty += $item->getQty();
 				}
 				$itemsCount += 1;
-				$itemsQty += (float) $item->getQty();
-        	}
+				$itemsQty += (float)$item->getQty();
+			}
 			$quote->setVirtualItemsQty($virtualItemsQty)
 				->setItemsCount($itemsCount)
 				->setItemsQty($itemsQty);
@@ -197,22 +286,22 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 
 	/**
 	 * "Unload" the specified catalog entity if the groupscatalog settings specify so
-	 * 
+	 *
 	 * @param Mage_Catalog_Model_Abstract $entity
 	 * @return void
 	 */
 	protected function _applyGroupsCatalogSettingsToEntity(Mage_Catalog_Model_Abstract $entity)
 	{
 		$helper = $this->_getHelper();
-		if ($helper->isModuleActive() && ! $this->_isApiRequest())
+		if ($helper->isModuleActive() && !$this->_isApiRequest())
 		{
-			if (! $helper->isEntityVisible($entity))
+			if (!$helper->isEntityVisible($entity))
 			{
 				$entity->setData(null)->setId(null);
 				// Set flag to make it easier to implement a redirect if needed (or debug)
 				$entity->setData('forbidden_by_groupscatalog2', true);
 				$entity->setData('forbidden_by_groupscatalog2_debug', array(
-					'method' => __METHOD__, 'file' => __FILE__, 'line' => (__LINE__ -4)
+					'method' => __METHOD__, 'file' => __FILE__, 'line' => (__LINE__ - 4)
 				));
 			}
 		}
@@ -227,7 +316,7 @@ class Netzarbeiter_GroupsCatalog2_Model_Observer
 	protected function _addGroupsCatalogFilterToCollection(Mage_Eav_Model_Entity_Collection_Abstract $collection)
 	{
 		$helper = $this->_getHelper();
-		if ($helper->isModuleActive() && ! $this->_isApiRequest())
+		if ($helper->isModuleActive() && !$this->_isApiRequest())
 		{
 			$customerGroupId = $helper->getCustomerGroupId();
 			Mage::getResourceSingleton('netzarbeiter_groupscatalog2/filter')
---
+++ app/code/community/Netzarbeiter/GroupsCatalog2/Model/System/Config/Source/HiddenEntityHandling.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Netzarbeiter
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category   Netzarbeiter
+ * @package    Netzarbeiter_GroupsCatalog2
+ * @copyright  Copyright (c) 2012 Vinai Kopp http://netzarbeiter.com
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Netzarbeiter_GroupsCatalog2_Model_System_Config_Source_HiddenEntityHandling
+{
+	const HIDDEN_ENTITY_HANDLING_NOROUTE = 404;
+	const HIDDEN_ENTITY_HANDLING_REDIRECT = 302;
+
+	public function toOptionArray()
+	{
+		$helper = Mage::helper('netzarbeiter_groupscatalog2');
+		return array(
+			array(
+				'value' => self::HIDDEN_ENTITY_HANDLING_NOROUTE,
+				'label' => $helper->__('Show 404 Page')
+			),
+			array(
+				'value' => self::HIDDEN_ENTITY_HANDLING_REDIRECT,
+				'label' => $helper->__('Redirect')
+			)
+		);
+	}
+}
