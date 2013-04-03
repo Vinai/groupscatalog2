c4cfe16a3700bf5623280e57f6de1a959948d967
Vinai Kopp <vinai@netzarbeiter.com>
2013-04-03 16:39:29 +0200

    Add missing licence header comments

--- app/code/community/Netzarbeiter/GroupsCatalog2/Test/Model/Observer.php
+++ app/code/community/Netzarbeiter/GroupsCatalog2/Test/Model/Observer.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Netzarbeiter
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category   Netzarbeiter
+ * @package    Netzarbeiter_GroupsCatalog2
+ * @copyright  Copyright (c) 2013 Vinai Kopp http://netzarbeiter.com
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 /**
  * @see Netzarbeiter_GroupsCatalog2_Model_Observer
