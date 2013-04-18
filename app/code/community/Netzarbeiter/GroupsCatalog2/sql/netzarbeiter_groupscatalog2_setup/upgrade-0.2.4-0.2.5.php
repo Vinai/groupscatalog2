7eac1189bd5a25e188307b53ef7fa054a77d95c3
Vinai Kopp <vinai@netzarbeiter.com>
2013-04-18 11:10:22 +0200

    Fix 0.2.5 upgrade script

--- app/code/community/Netzarbeiter/GroupsCatalog2/sql/netzarbeiter_groupscatalog2_setup/upgrade-0.2.4-0.2.5.php
+++ app/code/community/Netzarbeiter/GroupsCatalog2/sql/netzarbeiter_groupscatalog2_setup/upgrade-0.2.4-0.2.5.php
@@ -32,23 +32,35 @@ foreach (array('product', 'category') as $entity) {
     $tableAlias = 'netzarbeiter_groupscatalog2/' . $entity . '_index';
     $tableName = $installer->getTable($tableAlias);
 
-    // Drop existing index and foreign key constraint
-    $connection->dropIndex(
-        $tableName, $installer->getIdxName($tableName, array('entity_id', 'group_id', 'store_id'))
-    );
-    $connection->dropForeignKey(
-        $tableName, $installer->getFkName($tableName, 'entity_id', 'catalog/' . $entity, 'entity_id')
-    );
+    // Drop existing indexes and foreign key constraints.
+    // We should only need to drop the foreign keys referring to the entity_id field,
+    // but that actually doesn't seem to be enough in all cases (see
+    // https://github.com/Vinai/groupscatalog2/issues/44 )
+    $fk = $installer->getConnection()->getForeignKeys($tableName);
+    foreach ($fk as $info) {
+        $connection->dropForeignKey($tableName, $info['FK_NAME']);
+    }
+    $idx = $installer->getConnection()->getIndexList($tableName);
+    foreach ($idx as $info) {
+        if ('PRIMARY' != $info['KEY_NAME']) {
+            $connection->dropIndex($tableName, $info['KEY_NAME']);
+        }
+    }
 
-    // Change column entity_id to catalog_entity_id
-    $connection->changeColumn($tableName, 'entity_id', 'catalog_entity_id', array(
-        'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
-        'unsigned' => true,
-        'nullable' => false,
-        'comment' => ucwords($entity) . ' ID'
-    ));
+    $columns = $connection->describeTable($tableName);
+    if (! isset($columns['catalog_entity_id'])) {
+        // The table already was modified - this is just to make it easier to debug/re-run this script
 
-    // Recreate index and foreign key constraint to confirm with the new column name
+        // Change column entity_id to catalog_entity_id
+        $connection->changeColumn($tableName, 'entity_id', 'catalog_entity_id', array(
+            'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'unsigned' => true,
+            'nullable' => false,
+            'comment' => ucwords($entity) . ' ID'
+        ));
+    }
+
+    // Recreate indexes and foreign key constraints to confirm with the new column name
     $connection->addIndex(
         $tableName,
         $installer->getIdxName($tableName, array('catalog_entity_id', 'group_id', 'store_id')),
@@ -59,6 +71,16 @@ foreach (array('product', 'category') as $entity) {
         $installer->getFkName($tableName, 'catalog_entity_id', 'catalog/product', 'entity_id'),
         $tableName,
         'catalog_entity_id', $installer->getTable('catalog/' . $entity), 'entity_id'
+    );
+    $connection->addForeignKey(
+        $installer->getFkName($tableName, 'group_id', 'customer/customer_group', 'customer_group_id'),
+        $tableName,
+        'group_id', $installer->getTable('customer/customer_group'), 'customer_group_id'
+    );
+    $connection->addForeignKey(
+        $installer->getFkName($tableName, 'store_id', 'core/store', 'store_id'),
+        $tableName,
+        'store_id', $installer->getTable('core/store'), 'store_id'
     );
 }
 
